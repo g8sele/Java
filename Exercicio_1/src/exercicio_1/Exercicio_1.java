65716d59046fb15c7b17fe4a0def597a2e47ef92
Gisele Leite <156721087+g8sele@users.noreply.github.com>
2024-03-22 15:15:26 -0300

    Add files via upload

---
+++ Exercicio_1/src/exercicio_1/Exercicio_1.java
@@ -0,0 +1,19 @@
+package exercicio_1;
+
+public class Exercicio_1 {
+
+    public static void main(String[] args) {
+        String nome = "Nome: Gisele Leite.";
+        String idade = "Idade: 19 anos.";
+        String bairro = "Bairro: Coroa.";
+        String cidade = "Cidade: Vera Cruz-BA.";
+        String profissao = "Profissão: Estudante.";
+        
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(bairro);
+        System.out.println(cidade);
+        System.out.println(profissao);   
+    }
+    
+}
