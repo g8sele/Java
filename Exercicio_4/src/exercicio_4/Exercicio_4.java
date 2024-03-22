776e697a9acf11b59fc72a4d64bfeca28e905cc7
Gisele Leite <156721087+g8sele@users.noreply.github.com>
2024-03-22 15:17:09 -0300

    Add files via upload

---
+++ Exercicio_4/src/exercicio_4/Exercicio_4.java
@@ -0,0 +1,13 @@
+package exercicio_4;
+
+public class Exercicio_4 {
+
+    public static void main(String[] args) {
+         int quantiBalas = 6;
+         int pacoteUni = 32;
+         int compras = quantiBalas * pacoteUni;
+         
+         System.out.println("Beatriz comprou ao todo: "+ compras);
+    }
+    
+}
---
+++ Exercicio_5/src/exercicio_5/Exercicio_5.java
@@ -0,0 +1,13 @@
+package exercicio_5;
+
+public class Exercicio_5 {
+
+    public static void main(String[] args) {
+        int alunos = 26;
+        int balas = 208;
+        int divisao =  balas / alunos ;
+        
+        System.out.println("Cada aluno recebera: "+ divisao);
+    }
+    
+}
